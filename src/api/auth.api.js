7f7f0f754361bcd2f4c8b0031dbbb8476e3e5527
duynguyen <duynguyen382k7@gmail.com>
2026-01-03 14:10:55 +0700

    sửa lỗi thiếu /api ở auth.api.js

--- src/api/auth.api.js
+++ src/api/auth.api.js
@@ -1,9 +1,9 @@
 import api from "@/services/axios"
 
 export function loginApi(data) {
-  return api.post("/auth/login", data)
+  return api.post("/api/auth/login", data)
 }
 
 export function registerApi(data) {
-  return api.post("/auth/register", data)
+  return api.post("/api/auth/register", data)
 }
